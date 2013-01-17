c4bf5df0fb3012129593a497fd03e52c9813e76c
Nikita Nikitin <berozzy@gmail.com>
2013-01-17 16:25:42 +0200

    Nice flex footer with search

--- templates/footer.tmpl.php
+++ templates/footer.tmpl.php
@@ -1,15 +1,14 @@
 <? _X ('footer-pre') ?>
 <footer>
+	<div class="searchform">
+		<? _T_FOR ('form-search') ?>
+	</div>
 	<div class="copyrights">
 		<? _T ('copyrights') ?><br/>
 		<? if (!$content['sign-in']['done?'] and !$content['sign-in']['necessary?']) { ?>
 		<a id="e2-visual-login" href="<?= $content['hrefs']['sign-in'] ?>"><img src="<?= _IMGSRC ('login.png') ?>"/></a>
 		<? } ?>
 		<div class="engine-info"><? _T ('engine-info') ?></div>
-	</div>
-	
-	<div class="searchform">
-		<? _T_FOR ('form-search') ?>
 	</div>
 </footer>
 <? _X ('footer-post') ?>
--- templates/form-search.tmpl.php
+++ templates/form-search.tmpl.php
@@ -2,7 +2,7 @@
 <form  id="e2-form-search"  class="e2-enterable"  action="<?= @$content['form-search']['form-action'] ?>" method="post">
     <div class="flr">
         <img class="loupe" src="<?= _IMGSRC ('search.gif') ?>" width="16" height="16" align="absmiddle" onclick="searchSubmit();"/>
-        <input type="search" class="text" name="query" id="query" value="<?= @$content['form-search']['query'] ?>"/>
+        <input placeholder="Поиск по блогу" class="text" name="query" id="query" value="<?= @$content['form-search']['query'] ?>"/>
     </div>
 </form>
 <? } ?>
