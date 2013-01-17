6fecb5cfd9fa9169f62240130d22709923f72ac5
Nikita Nikitin <berozzy@gmail.com>
2013-01-17 18:44:50 +0200

    Making tags awesome!

--- templates/footer.tmpl.php
+++ templates/footer.tmpl.php
@@ -4,11 +4,11 @@
 		<? _T_FOR ('form-search') ?>
 	</div>
 	<div class="copyrights">
-		<? _T ('copyrights') ?><br/>
-		<? if (!$content['sign-in']['done?'] and !$content['sign-in']['necessary?']) { ?>
+		<? _T ('copyrights') ?>
+	<? if (!$content['sign-in']['done?'] and !$content['sign-in']['necessary?']) { ?>
 		<a id="e2-visual-login" href="<?= $content['hrefs']['sign-in'] ?>"><img src="<?= _IMGSRC ('login.png') ?>"/></a>
 		<? } ?>
-		<div class="engine-info"><? _T ('engine-info') ?></div>
+	<div class="engine-info"><? _T ('engine-info') ?></div>
 	</div>
 </footer>
 <? _X ('footer-post') ?>
